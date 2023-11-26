123474971ad53cd55ac63984a37a3126718a1901
Daniel Fonseca <101329358+Daniel-Fonseca-S@users.noreply.github.com>
2023-11-25 23:21:12 -0300

    Co-authored-by: davi takayama <davi.takayama.vaz@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/accarto-logo.png';
 
 function App() {
+
+  // const [imagem, setImagem] = React.useState<File | undefined>(undefined);
+  const [imagem, setImagem] = React.useState<any | File | undefined>(null);
+
+  function handleLogoClick() {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  };
+
+  function handleEnviarImagem() {
+    console.log(imagem);
+  };
+
+  const onImageChange = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      setImagem(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <main className='container'>
+        <section className='cabecalho'>
+          <img src={logo} alt="logo do Accarto" className='logo' onClick={handleLogoClick} />
+          <h1 className='App-header'>Accarto - Identificador de veículos</h1>
+        </section>
+        <section className='form'>
+          <p className='form-header'>Selecione uma imagem de um veículo e clique em "Enviar Imagem".</p>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={(e) => onImageChange(e)}
+            className='file-selector' placeholder='selecione uma imagem'
+          />
+
+          {imagem && <img src={imagem} alt="arquivo de imagem" className='imagem' />}
+
+          <button onClick={handleEnviarImagem} className='button'>Enviar Imagem</button>
+        </section>
+      </main>
     </div>
   );
 }
